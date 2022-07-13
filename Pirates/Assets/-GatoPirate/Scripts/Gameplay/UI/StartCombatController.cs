70c1596afb4ee3850f65372e34615de06ce1b43a
Eduardo Arroyo <eduardo.arroyo177@gmail.com>
2022-07-13 17:34:55 -0500

    Skip intro button hidden when combat started

--- Pirates/Assets/-GatoPirate/Scripts/Gameplay/UI/StartCombatController.cs
+++ Pirates/Assets/-GatoPirate/Scripts/Gameplay/UI/StartCombatController.cs
@@ -8,6 +8,8 @@ public class StartCombatController : MonoBehaviour
 {
     [SerializeField]
     private float startCombatDelay;
+    [SerializeField]
+    private GameObject btn_skipIntro;
     public VoidEvent StartCombatEvent { get; set; }
     public VoidEvent StartingAnimationsFinishedEvent { get; set; }
 
@@ -31,6 +33,7 @@ public class StartCombatController : MonoBehaviour
     private void StartCombatDelayed()
     {
         StartCombatEvent.Raise();
+        btn_skipIntro.SetActive(false);
         gameObject.SetActive(false);
     }
 

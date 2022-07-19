d79e20cbb3d3109fcf8b0e16f6ddaf2501d69129
Eduardo Arroyo <eduardo.arroyo177@gmail.com>
2022-07-18 21:38:56 -0500

    Island scroll pt. 1 implemented

--- Pirates/Assets/-GatoPirate/Scripts/MetaGame/IslandScrollController.cs
+++ Pirates/Assets/-GatoPirate/Scripts/MetaGame/IslandScrollController.cs
@@ -5,68 +5,78 @@ using UnityEngine.EventSystems;
 
 public class IslandScrollController : MonoBehaviour
 {
-    public float movementSpeed;
-    public float dragDelay;
+    public float mousePositionDistance;
 
-    public Vector3 lastPosition;
-    public Vector3 newPosition;
+    //public Vector3 lastPosition;
+    //public Vector3 mouseWorldPointPosition;
 
-    public Vector3 mousePosition;
-    public Vector3 lastMousePosition;
-    private bool isDragging;
+    //public Vector3 mousePosition;
+    //public Vector3 lastMousePosition;
+
+    //public Vector3 normalizedPosition;
+    //private bool isDragging;
+
+    public Vector3 screenPoint;
+    public Vector3 offset;
+    // private Vector3 originalPosition;
+    public Vector3 cursorPoint;
+    public Vector3 cursorPosition;
 
     private void Start()
     {
-        lastPosition = transform.position;
+        //originalPosition = transform.position;
     }
 
     void OnMouseDown()
     {
-        lastMousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-        //StartCoroutine("CheckIfDragging");
+        //lastMousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mousePositionDistance);
+        // posicion init = posición actual
+        // parent = parent actual
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Debug.Log("MOUSE DOWN");
-    }
-
-    private IEnumerator CheckIfDragging()
-    {
-        yield return new WaitForSeconds(dragDelay);
-        isDragging = true;
-    }
+        }
 
     void OnMouseDrag()
     {
-        //if(isDragging)
-            ScrollIsland();
+        ScrollIsland();
     }
 
     private void ScrollIsland()
     {
-        mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-        if (lastMousePosition != mousePosition)
-        {
-            lastMousePosition = mousePosition;
-            newPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition; //new Vector3(cursorPosition.x, cursorPosition.y, transform.position.z);
 
-            //Debug.Log("MOUSE DRAGGING");
-        }
+        //mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mousePositionDistance);
+
+        //if (lastMousePosition != mousePosition)
+        //{
+        //    isDragging = true;
+        //    mouseWorldPointPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //    transform.position = mouseWorldPointPosition;
+            
+            //normalizedPosition = mouseWorldPointPosition.normalized;
+            //transform.position = new Vector3(lastPosition.x + mouseWorldPointPosition.x,
+            //transform.position.y,
+            //transform.position.z);
+        //}
     }
 
     private void OnMouseUp()
     {
-        //StopCoroutine("CheckIfDragging");
+        //lastMousePosition = mousePosition;
         //isDragging = false;
         //lastPosition = transform.position;
-        Debug.Log("MOUSE UP");
     }
 
     private void Update()
     {
         //if (isDragging)
         //{
-        //    transform.position = new Vector3(lastPosition.x + newPosition.x,
-        //        transform.position.y,
-        //        transform.position.z);
+        //    //transform.position = new Vector3(mouseWorldPointPosition.x,
+        //    //transform.position.y,
+        //    //transform.position.z);
         //}
     }
 
---
+++ Pirates/Assets/Testing/Scripts/DragObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    public Vector3 offset;
+    public float objectDepth;
+
+    void OnMouseDown()
+    {
+        objectDepth = Camera.main.WorldToScreenPoint(transform.position).z;
+        offset = transform.position - GetMouseAsWorldPoint();
+    }
+
+
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = objectDepth;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+
+
+    void OnMouseDrag()
+    {
+        Vector3 mouseWorldPoint = GetMouseAsWorldPoint();
+        transform.position = new Vector3(mouseWorldPoint.x + offset.x, transform.position.y, transform.position.z);
+    }
+}

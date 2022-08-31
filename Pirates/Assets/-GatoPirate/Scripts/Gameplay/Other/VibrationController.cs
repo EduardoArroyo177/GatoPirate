169ae4cbebab8b12402cb9d8d0193d5b9cb6f22a
Eduardo Arroyo <eduardo.arroyo177@gmail.com>
2022-08-31 13:50:34 -0500

    Vibration controller updated

--- Pirates/Assets/-GatoPirate/Scripts/Gameplay/Other/VibrationController.cs
+++ Pirates/Assets/-GatoPirate/Scripts/Gameplay/Other/VibrationController.cs
@@ -1,17 +1,35 @@
 using Lofelt.NiceVibrations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 public class VibrationController : SceneSingleton<VibrationController>
 {
+    private bool isVibrationOn = false;
+
+    public void Initialize()
+    {
+        isVibrationOn = SettingsDataSaveManager.Instance.GetVibrationOn();
+    }
+
+    public void TriggerVibrationOn()
+    {
+        HapticPatterns.PlayPreset(HapticPatterns.PresetType.SoftImpact);
+    }
+
     public void TriggerBasicAttackVibration()
     {
+        if (!isVibrationOn)
+            return;
+
         HapticPatterns.PlayPreset(HapticPatterns.PresetType.LightImpact);
     }
 
     public void TriggerNormalAttackVibration()
     {
+        if (!isVibrationOn)
+            return;
         //HapticController.fallbackPreset = HapticPatterns.PresetType.LightImpact;
         //HapticPatterns.PlayEmphasis(0.05f, 0.05f);
         HapticPatterns.PlayPreset(HapticPatterns.PresetType.LightImpact);
@@ -19,17 +37,23 @@ public class VibrationController : SceneSingleton<VibrationController>
 
     public void TriggerReceiveNormalAttackVibration()
     {
-
+        if (!isVibrationOn)
+            return;
     }
 
     public void TriggerSpecialAttackVibration()
     {
-        HapticPatterns.PlayPreset(HapticPatterns.PresetType.SoftImpact);
+        if (!isVibrationOn)
+            return;
 
+        HapticPatterns.PlayPreset(HapticPatterns.PresetType.SoftImpact);
     }
 
     public void TriggerReceiveSpecialAttackVibration(float _duration)
     {
+        if (!isVibrationOn)
+            return;
+
         HapticController.Stop();
         HapticController.fallbackPreset = HapticPatterns.PresetType.HeavyImpact;
         HapticPatterns.PlayConstant(0.85f, 0.5f, _duration);
